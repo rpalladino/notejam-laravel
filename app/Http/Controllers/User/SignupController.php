0ab1d98e80320b83c68e4536b1acf1054a436595
Rocco Palladino <rpalladino@gmail.com>
2017-01-02 18:20:39 -0600

    When a user signs up, they should be logged in automatically

--- app/Http/Controllers/User/SignupController.php
+++ app/Http/Controllers/User/SignupController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\User;
 use App\User;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\User\SignupRequest;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class SignupController extends Controller
@@ -20,6 +21,8 @@ class SignupController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password)
         ]);
+
+        Auth::login($user);
 
         return redirect()
             ->route('all_notes')
