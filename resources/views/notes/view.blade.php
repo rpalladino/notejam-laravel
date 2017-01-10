cc0f25ee0e9b4e490d29cd05b9d2591396c8f576
Rocco Palladino <rpalladino@gmail.com>
2017-01-09 23:49:15 -0600

    Display note text with line breaks

--- resources/views/notes/view.blade.php
+++ resources/views/notes/view.blade.php
@@ -5,7 +5,9 @@
 @section('content')
   <p class="hidden-text">Last edited at {{ $note->updated_at->diffForHumans() }}</p>
   <div class="note">
-      {{ $note->text }}
+    <p>
+        {!! nl2br($note->text) !!}
+    </p>
   </div>
   <button type="button">Edit</button>
   <a href="#" class="delete-note">Delete it</a>
