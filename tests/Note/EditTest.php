6711136d8f8ec965b311a026667dcd084a323bb0
Rocco Palladino <rpalladino@gmail.com>
2017-01-14 07:23:02 -0600

    Refactor test to set up necessary context before each test

--- tests/Note/EditTest.php
+++ tests/Note/EditTest.php
@@ -3,10 +3,33 @@
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\User;
+use App\Note;
 
 class EditTest extends TestCase
 {
     use DatabaseMigrations;
+
+    /**
+     * The user who owns the note.
+     * @var User
+     */
+    private $owner;
+
+    /**
+     * The note being edited.
+     * @var Note
+     */
+    private $note;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->owner = factory(User::class)->create();
+        $this->note = factory(Note::class)->create([
+            'user_id' => $this->owner->id
+        ]);
+    }
 
     /**
      * Note can be edited by its owner
@@ -15,21 +38,16 @@ class EditTest extends TestCase
      */
     public function testNoteCanBeEditedByItsOwner()
     {
-        $owner = factory(App\User::class)->create();
-        $note = factory(App\Note::class)->create([
-            'user_id' => $owner->id
-        ]);
-
-        $this->actingAs($owner)
-             ->visit("/notes/{$note->id}")
+        $this->actingAs($this->owner)
+             ->visit("/notes/{$this->note->id}")
              ->click('Edit')
-             ->seePageIs("/notes/{$note->id}/edit")
-             ->see($note->name)
-             ->see($note->text)
+             ->seePageIs("/notes/{$this->note->id}/edit")
+             ->see($this->note->name)
+             ->see($this->note->text)
              ->type('New name', 'name')
              ->type('New text', 'text')
              ->press('Save')
-             ->seePageIs("/notes/{$note->id}")
+             ->seePageIs("/notes/{$this->note->id}")
              ->see('Note is successfully updated')
              ->see('New name')
              ->see('New text');
