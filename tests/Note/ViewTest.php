d5ef967ef277cb980fb02e9f35ad816b979d4a62
Rocco Palladino <rpalladino@gmail.com>
2017-01-19 06:08:01 -0600

    Note can't be viewed by not an owner

--- tests/Note/ViewTest.php
+++ tests/Note/ViewTest.php
@@ -3,31 +3,63 @@
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Http\Response;
+use App\Note;
+use App\User;
 
 class ViewTest extends TestCase
 {
     use DatabaseMigrations;
 
     /**
+     * The note being viewed.
+     * @var Note
+     */
+    private $note;
+
+    /**
+     * The user who owns the note.
+     * @var User
+     */
+    private $owner;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->owner = factory(User::class)->create();
+        $this->note = factory(Note::class)->create([
+            'user_id' => $this->owner->id
+        ]);
+    }
+
+    /**
      * Note can successfully be viewed
      *
      * @return void
      */
-    public function testNoteCanBeSuccessfullyViewed()
+    public function testNoteCanBeViewedByItsOwner()
     {
-        $user = factory(App\User::class)->create();
-        $note = factory(App\Note::class)->create([
-            'user_id' => $user->id
-        ]);
-
-        $this->actingAs($user)
+        $this->actingAs($this->owner)
              // Click note name from list
              ->visit('/')
-             ->click($note->name)
+             ->click($this->note->name)
             // Should transition to view note route
-             ->seePageIs("/notes/{$note->id}")
+             ->seePageIs("/notes/{$this->note->id}")
              // Should see note name and text
-             ->see($note->name)
-             ->see($note->text);
+             ->see($this->note->name)
+             ->see($this->note->text);
+    }
+
+    /**
+     * Note can't be viewed by not an owner
+     *
+     * @return void
+     */
+    public function testNoteCantBeViewedByNotAnOwner()
+    {
+        $notAnOwner = factory(User::class)->create();
+        $this->actingAs($notAnOwner)
+             ->get("/notes/{$this->note->id}")
+             ->assertResponseStatus(Response::HTTP_FORBIDDEN);
     }
 }
